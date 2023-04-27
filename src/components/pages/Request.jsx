24207aab207be784a3f23cef4ac780a6b01a0941
David Cruz <131717928+david-a-cruz@users.noreply.github.com>
2023-04-26 18:32:27 -0600

    Ajuste en Request.jsx

--- src/components/pages/Request.jsx
+++ src/components/pages/Request.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles/Request.css';
 
 const Request = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
    <div className="text-white" id="contact">

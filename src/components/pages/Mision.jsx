1f24576786685df5a892cbcab751a8bd5c3846f9
David Cruz <54298536+cruzito-exe@users.noreply.github.com>
2023-05-03 17:50:46 -0600

    mision and vision fixed to mobile

--- src/components/pages/Mision.jsx
+++ src/components/pages/Mision.jsx
@@ -10,11 +10,15 @@ const Mision = () => {
     <div className="container my-5">
      <div className="row justify-content-center">
       <div className="col-md-9">
-       <h1 className="text-center mt-5 my-5 text-white"> Misión </h1>
-       <p className="mt-5 pt-5 mb-5">  Ofrecer productos de repostería de alta calidad, utilizando tanto ingredientes frescos y naturales como la mejor implementación de maquinarías posible para satisfacer las necesidades y gustos de nuestros clientes. Nos comprometemos a innovar constantemente y a mejorar nuestros procesos para ofrecer productos frescos, deliciosos y nutritivos. Además, nos esforzamos por ofrecer un excelente servicio al cliente, entregando nuestros productos a tiempo y asegurando la satisfacción del cliente en cada interacción. <br /> <br />
+       <div className="card mision rounded mt-5 my-5 text-center">
+        <h1 className=""> Misión </h1>
+        <p className="mb-5">  Ofrecer productos de repostería de alta calidad, utilizando tanto ingredientes frescos y naturales como la mejor implementación de maquinarías posible para satisfacer las necesidades y gustos de nuestros clientes. Nos comprometemos a innovar constantemente y a mejorar nuestros procesos para ofrecer productos frescos, deliciosos y nutritivos. Además, nos esforzamos por ofrecer un excelente servicio al cliente, entregando nuestros productos a tiempo y asegurando la satisfacción del cliente en cada interacción. </p>
+       </div>
 
-       <h1 className="text-center mt-5 my-5 text-white"> Visión </h1>
-       Convertirnos en la empresa líder en producción y distribución de repostería a nivel regional. Nos esforzamos por mantener la calidad y la frescura de nuestros productos, así como por ofrecer una amplia variedad de opciones para satisfacer los gustos y necesidades de nuestros clientes. Además, nos comprometemos a ser un negocio socialmente responsable y sostenible, minimizando nuestro impacto ambiental. Queremos ser reconocidos como la mejor opción para aquellos que buscan productos de repostería de alta calidad y un servicio excepcional. </p>
+       <div className="card mision mt-3 rounded mt-5 my-5 text-center">
+        <h1 className=""> Visión </h1>
+        <p> Convertirnos en la empresa líder en producción y distribución de repostería a nivel regional. Nos esforzamos por mantener la calidad y la frescura de nuestros productos, así como por ofrecer una amplia variedad de opciones para satisfacer los gustos y necesidades de nuestros clientes. Además, nos comprometemos a ser un negocio socialmente responsable y sostenible, minimizando nuestro impacto ambiental. Queremos ser reconocidos como la mejor opción para aquellos que buscan productos de repostería de alta calidad y un servicio excepcional. </p>
+       </div>
       </div>
      </div>
     </div>

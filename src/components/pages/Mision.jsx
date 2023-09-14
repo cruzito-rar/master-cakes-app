9c496093a4cd29d454742ba52d21ae84718dc1d7
David Cruz <dcruzer92@gmail.com>
2023-09-14 00:36:04 -0600

    Inicios en ajuste de reponsive

--- src/components/pages/Mision.jsx
+++ src/components/pages/Mision.jsx
@@ -9,15 +9,15 @@ const Mision = () => {
     <div>
     <div className="container my-5">
      <div className="row justify-content-center">
-      <div className="col-md-9">
-       <div className="card mision rounded mt-5 my-5 ">
+      <div className="d-flex d-sm-12 col-lg-12 col-md-9 col-sm-12">
+       <div className="card mision rounded mt-4 my-5 w-50 me-3 col-sm-12">
         <h1 className="my-3"> Misión </h1>
         <p className="text-start mb-4 text-none-v">  Ofrecer productos de repostería de alta calidad, utilizando tanto ingredientes frescos y naturales como la mejor implementación de maquinarías posible para satisfacer las necesidades y gustos de nuestros clientes. Nos comprometemos a innovar constantemente y a mejorar nuestros procesos para ofrecer productos frescos, deliciosos y nutritivos. Además, nos esforzamos por ofrecer un excelente servicio al cliente, entregando nuestros productos a tiempo y asegurando la satisfacción del cliente en cada interacción. </p>
        </div>
 
-       <div className="card mision mt-3 rounded mt-4 my-5">
-        <h1> Visión </h1>
-        <p className="text-start text-none-v"> Convertirnos en la empresa líder en producción y distribución de repostería a nivel regional. Nos esforzamos por mantener la calidad y la frescura de nuestros productos, así como por ofrecer una amplia variedad de opciones para satisfacer los gustos y necesidades de nuestros clientes. Además, nos comprometemos a ser un negocio socialmente responsable y sostenible, minimizando nuestro impacto ambiental. Queremos ser reconocidos como la mejor opción para aquellos que buscan productos de repostería de alta calidad y un servicio excepcional. </p>
+       <div className="card mision rounded mt-4 my-5 w-50 ms-3 col-sm-12">
+        <h1 className="my-3"> Visión </h1>
+        <p className="text-start mb-4 text-none-v"> Convertirnos en la empresa líder en producción y distribución de repostería a nivel regional. Nos esforzamos por mantener la calidad y la frescura de nuestros productos, así como por ofrecer una amplia variedad de opciones para satisfacer los gustos y necesidades de nuestros clientes. Además, nos comprometemos a ser un negocio socialmente responsable y sostenible, minimizando nuestro impacto ambiental. Queremos ser reconocidos como la mejor opción para aquellos que buscan productos de repostería de alta calidad y un servicio excepcional. </p>
        </div>
       </div>
      </div>
